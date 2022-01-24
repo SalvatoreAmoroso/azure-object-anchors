4b0a810886aecd2b17b549213016303616fc7d15
Salvatore Amoroso <salvatore.amoroso@sew-eurodrive.de>
2022-01-24 09:25:47 +0100

    Delete DirectX project

--- quickstarts/apps/directx/AoaSampleApp/pch.cpp
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT license.
-//
-// pch.cpp
-// Include the standard header and generate the precompiled header.
-//
-
-#include "pch.h"
