8a87eed65b31a163d2bcb0008b66842701d0cf41
Salvatore Amoroso <amo_salvatore@web.de>
2022-01-24 11:42:58 +0100

    Add DebugWindow

---
+++ quickstarts/apps/unity/mrtk/Assets/DebugWindow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class DebugWindow : MonoBehaviour
+{
+    public TextMeshPro textMesh;
+
+
+    void OnEnable()
+    {
+        Application.logMessageReceived += LogMessage;
+    }
+
+    void OnDisable()
+    {
+        Application.logMessageReceived -= LogMessage;
+    }
+
+    public void LogMessage(string message, string stackTrace, LogType type)
+    {
+        if (textMesh.text.Length > 300)
+        {
+            textMesh.text = message + "\n";
+        }
+        else
+        {
+            textMesh.text += message + "\n";
+        }
+    }
+}
